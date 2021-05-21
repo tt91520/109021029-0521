bad3808145015295e7154f3155631d0c1d32b2d5
tt91520 <109021029@live.asia.edu.tw>
2021-05-21 17:01:25 +0800

    0521

--- demo3.py
+++ demo3.py
@@ -11,9 +11,13 @@ drawObj.line([(3, 3), (296, 3), (296, 296), (3, 296), (3,3)], fill="#0000ff")
 drawObj.line([(13, 13), (283, 13), (283, 283), (13, 283), (13,13)], fill="#0000ff")
 drawObj.line([(23, 23), (273, 23), (273, 273), (23, 273), (23,23)], fill="#0000ff")
 
-for x in range(150, 300, 10):
-    drawObj.line([(x, 0), (300, x-150)],  fill="#000000")
-for y in range(0, 300, 10):
-    drawObj.line([(y, 0), (y-150, 0)],  fill="#000000")
+for x in range(150, 300, 10): #右上
+    drawObj.line([(x, 0), (300, x-150)],  fill="#000000") #(起始), (終點)
+for y in range(150, 300, 10): #左下
+    drawObj.line([(0, y), (y-150, 300)],  fill="#000000")
+for z in range(150, 300, 10): #右下
+    drawObj.line([(z, 300), (300, 450-z)],  fill="#000000")
+for q in range(0, 150, 10): #左上
+    drawObj.line([(q, 0), (0, 150-q)],  fill="#000000")
 
 newImg.save("testImag.png")
